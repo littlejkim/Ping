b2fd1109618767784a7bfb5e7bac00e2388bfe5b
김영도 <younkng@gim-yeongdos-MacBook-Pro.local>
2020-06-16 00:33:07 +0900

    this is hard as f

--- App.js
+++ App.js
@@ -1,53 +1,10 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {Text, View} from 'react-native';
 
-function HomeScreen({navigation}) {
+export default function App({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Move to Details Screen"
-        onPress={() => navigation.navigate('Details')}
-      />
+    <View>
+      <Text>Fuck</Text>
     </View>
   );
 }
-
-function DetailsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'normal',
-          },
-        }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
-});
-
-export default App;
